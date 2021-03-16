bd1e8c6246de1cea33f460c96ec85cd490fc5984
deb <bykovd1984@gmail.com>
2021-03-16 22:15:14 +0300

    add StudentController

---
+++ Lab 1.5/Lab 1.5/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_1._5.Controllers
+{
+    [ApiController]
+    public class StudentController : Controller
+    {
+        [HttpGet("student/{studentName}")]
+        [HttpGet("student/{studentName}/{*path}")]
+        public IActionResult Index(string studentName, string path)
+        {
+            return Ok(studentName);
+        }
+    }
+}
